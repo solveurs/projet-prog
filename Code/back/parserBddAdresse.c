555aff496519758d0689242252f4c399c0eadb0e
vivelespoireaux <vincent.malenfer-henard@insa-cvl.fr>
2016-11-04 11:08:37 +0100

    Intégration fonction récupérer_champ
    main interne toujours présent pour les tests, pas encore utilisable

--- Code/back/parserBddAdresse.c
+++ Code/back/parserBddAdresse.c
@@ -1,1 +1,35 @@
 #include "../headers/parserBddAdresse.h"
+/**
+ * \fn void const char* recuperer_champ(char* ligne, int numero_champ)
+ * \brief Récupère un champ dans un fichier csv
+ * \param ligne numéro de ligne
+ * \param numero_champ numéro de champ
+ */
+ 
+const char* recuperer_champ(char* ligne, int numero_champ)
+{
+  const char* tok;
+  for (tok = strtok(ligne, ",");tok && *tok;tok = strtok(NULL, ",\n")) // strtok écrit sur ligne
+  {
+    if (!--numero)
+    {
+      return tok;
+    }
+  }
+  return NULL;
+}
+ 
+int main()
+{
+  char ligne[1024];
+  while (fgets(ligne, 1024, stdin))
+  {
+    char* temp = strdup(ligne);
+    const char X=recuperer_champ(temp, 13);
+    const char Y=recuperer_champ(temp, 14);
+    printf("Coordonnees en lambert, %s %s", X, Y);
+    free(temp);
+  }
+} 
+
+
