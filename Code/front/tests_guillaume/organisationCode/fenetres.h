9c1e902196ebda83dff4c90b56992dc6800f09c8
GuillaumeFrogerSegret <ptitguigui41@hotmail.fr>
2016-12-11 23:38:49 +0100

    - Debut de reflexion sur l'organisation du code (cote front)

---
+++ Code/front/tests_guillaume/organisationCode/fenetres.h
@@ -0,0 +1,33 @@
+
+
+
+typedef struct fenetre_principale
+{
+  GtkWidget *boxBarreMenu;
+  GtkWidget *buttonGestionCartes;
+  GtkWidget *buttonGestionTraces;
+  GtkWidget *buttonGestionAnonymat;
+
+
+  GtkWidget *boxImage;
+  GtkWidget *Image;
+  GtkWidget * scroll;
+
+} fenetre_principale;
+
+
+
+
+typedef struct fenetre_gestionCartes
+{
+  GtkWidget *boxBarreMenu;
+  GtkWidget *buttonGestionCartes;
+  GtkWidget *buttonGestionTraces;
+  GtkWidget *buttonGestionAnonymat;
+
+
+  GtkWidget *boxImage;
+  GtkWidget *Image;
+  GtkWidget * scroll;
+
+} fenetre_gestionCartes;
