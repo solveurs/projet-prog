bc9cfeb55e29cb9428ce7fdf4952bb0b8fedfa5a
Thanh <tlutru@gmail.com>
2016-12-30 18:54:50 +0100

    [Front] Ajout d'une structure de variables globales

---
+++ Code/headers/globalFront.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "includes.h"
+#include "structures.h"
+#include "carte.h"
+
+/* ====== CONSTANTES ====== */
+#define NOMBRE_MAX_TRAJETS	32
+
+/* ====== Var. Glob ====== */
+struct
+{
+	trajet*			trajet[NOMBRE_MAX_TRAJETS];
+	int				idTrajet;
+
+	GtkWidget*		zoneDessinCher[NOMBRE_MAX_TRAJETS];
+	GtkWidget*		zoneDessinBourges[NOMBRE_MAX_TRAJETS];
+	GtkWidget*		zoneDessinInsa[NOMBRE_MAX_TRAJETS];
+
+	GtkWidget   	*overlayCarteCher;
+  	GtkWidget   	*overlayCarteBourges;
+  	GtkWidget   	*overlayCarteInsa;
+
+  	carte 			carte_Cher;
+  	carte 			carte_Bourges;
+  	carte 			carte_Insa;
+}globFront;
