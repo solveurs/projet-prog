8b7f6d1468c3acca708ac870cd8d991643baf0db
Thanh <tlutru@gmail.com>
2016-12-30 19:00:01 +0100

    [Front] Ajout des fonctions relatives a l'affichage des traces (sans prise en compte du temps)

---
+++ Code/front/fctnCairo.c
@@ -0,0 +1,109 @@
+#include "../headers/fctnCairo.h"
+
+void faire_tracesCher(GtkWidget *widget, cairo_t *cr, gpointer user_data)
+{
+  tracesItem* ptrTmp = (tracesItem *)user_data;
+  faire_tracer(cr, 0, ptrTmp->ptrTrajet);
+}
+
+void faire_tracesBourges(GtkWidget *widget, cairo_t *cr, gpointer user_data)
+{
+  tracesItem* ptrTmp = (tracesItem *)user_data;
+  faire_tracer(cr, 1, ptrTmp->ptrTrajet);
+}
+
+void faire_tracesInsa(GtkWidget *widget, cairo_t *cr, gpointer user_data)
+{
+  tracesItem* ptrTmp = (tracesItem *)user_data;
+  faire_tracer(cr, 2, ptrTmp->ptrTrajet);
+}
+
+void faire_tracer(cairo_t *cr, int carte, trajet *cible)
+{
+  double x;
+  double y;
+  // Epaisseur des lignes
+  cairo_set_line_width(cr, 1.0);
+  // Couleur de notre point de depart ici rouge
+  cairo_set_source_rgb(cr, 1, 0, 0);
+
+  // Tracer des points
+  trace* ptr;
+
+  // Traces en rouge
+  cairo_set_source_rgb(cr, 1, 0, 0);
+  for(ptr=cible->premier; ptr!=cible->dernier; ptr=ptr->suiv)
+  {
+    if(ptr->visibilite==1 /* && condition date */)
+    {
+      x = echelle(ptr->coord.x, carte, 1);
+      y = echelle(ptr->coord.y, carte, 0);
+
+      // Dessin d'un petit "+"
+      cairo_move_to(cr, x, y - TAILLE_TRACE);
+      cairo_line_to(cr, x, y + TAILLE_TRACE);
+
+      cairo_move_to(cr, x - TAILLE_TRACE, y);
+      cairo_line_to(cr, x + TAILLE_TRACE, y);
+
+      if(cible->visibilite) //si le trajet est visible
+      {
+        cairo_move_to(cr, x, y);
+        x = echelle(ptr->suiv->coord.x, carte, 1);
+        y = echelle(ptr->suiv->coord.y, carte, 0);
+        cairo_line_to(cr, x, y);
+      }
+    }
+  }
+  if(ptr->visibilite==1 /* && condition date */)
+  {
+    x = echelle(cible->dernier->coord.x, carte, 1);
+    y = echelle(cible->dernier->coord.y, carte, 0);
+    cairo_move_to(cr, x, y - TAILLE_TRACE);
+    cairo_line_to(cr, x, y + TAILLE_TRACE);
+
+    cairo_move_to(cr, x - TAILLE_TRACE, y);
+    cairo_line_to(cr, x + TAILLE_TRACE, y);
+  }
+
+  cairo_stroke_preserve(cr);
+}
+
+void faire_aggregation(cairo_t *cr, cercle *cercle)
+{
+  //
+}
+
+double echelle(double valeur, int carte, int longitude)
+{
+  switch(carte)
+  {
+    double conversion;
+    case 0: // Cher
+      if(longitude)
+      {
+        conversion = ((valeur - 1.726227) * TAILLE_X_CHER / 1.389770);
+        return conversion;
+      }
+      conversion = (-1.0 * (valeur - 47.988083) * TAILLE_Y_CHER / 1.793991);
+      return conversion;
+
+    case 1: // Bourges
+      if(longitude)
+      {
+        conversion = ((valeur - 2.313738) * TAILLE_X_BOURGES / 0.158872);
+        return conversion;
+      }
+      conversion = (-1.0 * (valeur - 47.113540) * TAILLE_Y_BOURGES / 0.063064);
+      return conversion;
+
+    default: // Insa
+      if(longitude)
+      {
+        conversion = ((valeur - 2.392981) * TAILLE_X_INSA / 0.039783);
+        return conversion;
+      }
+      conversion = (-1.0 * (valeur - 47.088723) * TAILLE_Y_INSA / 0.015766);
+      return conversion;
+  }
+}
---
+++ Code/headers/fctnCairo.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "includes.h"
+#include "structures.h"
+#include "uiTraces.h"
+
+/* ====== CONSTANTES ====== */
+#define TAILLE_TRACE           5.0
+#define TAILLE_X_CHER       1013.0
+#define TAILLE_Y_CHER       1920.0
+#define TAILLE_X_BOURGES    1850.0
+#define TAILLE_Y_BOURGES    1079.0
+#define TAILLE_X_INSA       1850.0
+#define TAILLE_Y_INSA       1080.0
+
+/* ===== Fonctions ===== */
+void faire_tracesCher(GtkWidget *widget, cairo_t *cr, gpointer user_data);
+void faire_tracesBourges(GtkWidget *widget, cairo_t *cr, gpointer user_data);
+void faire_tracesInsa(GtkWidget *widget, cairo_t *cr, gpointer user_data);
+void faire_tracer(cairo_t *cr, int carte, trajet *cible);
+
+void faire_aggregation(cairo_t *cr, cercle *cercle);
+
+double echelle(double valeur, int carte, int abscisse);
