eafea75a73c74a4c45cdf9a957e8c4340dbf8b6a
vivelespoireaux <vincent.malenfer-henard@insa-cvl.fr>
2017-01-05 11:01:27 +0100

    Ajout fonction readAddr
    Ajout de la fonction readAddr et mise en commentaire de la fonction recuperer_champ, non déclarée dans parserBddAdresse.h

--- Code/back/parserBddAdresse.c
+++ Code/back/parserBddAdresse.c
@@ -22,17 +22,37 @@ bdd_trace * readGeoloc(FILE * parFd)
 {
 	
 }*/
-const char* recuperer_champ(char* ligne, int numero_champ)
+bdd_addr * readAddr(FILE * fd)
+{
+adresse ** varBddAddr=(adresse**)malloc(sizeof(adresse));
+int compteurvirgule = 0;
+adresse varAddr; //avant insertion dans la BDD
+
+        while (fscanf(fd,"id:%s,id_tr:%s,methode:%s,numero:%i,rep:%s,compl:%s,nom_voie:%s,alias:%s,nom_ld:%s,insee:%i,c_postal:%i,type:%s,cote:%s,x_lambert:%i,y_lambert:%f" ) > 0)
+        {
+                vardAddr.numero=numero;
+                varAddr.nom_rue=nom_voie;
+                strncat(var.Addr.nom_rue,alias);
+                if (strncmp(nom_voie,nom_ld) != 0)
+                {
+                        strncat(var.Addr.nom_rue,nom_ld);
+                }
+                varAddr.code_postal=c_postal;
+        }
+
+
+
+/* const char* recuperer_champ(char* ligne, int numero_champ)
 {
 	
-//  const char* tok;
-//  for (tok = strtok(ligne, ",");tok && *tok;tok = strtok(NULL, ",\n")) // strtok écrit sur ligne
-//  {
-//    //if (!--numero)
-//    {
-//      return tok;
-//    }
-//  }
-//  return NULL;
+  const char* tok;
+  for (tok = strtok(ligne, ",");tok && *tok;tok = strtok(NULL, ",\n")) // strtok écrit sur ligne
+  {
+    //if (!--numero)
+    {
+      return tok;
+    }
+  }
+  return NULL;
 	
-}
+}*/
